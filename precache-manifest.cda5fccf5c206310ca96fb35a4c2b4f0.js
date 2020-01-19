0ba59d6d3370314a07010c6de72dff638caa2df5
Hassaine abdel-illah <hassaineabdelillah@gmail.com>
2020-01-19 22:46:52 +0100

    Updates

---
+++ precache-manifest.cda5fccf5c206310ca96fb35a4c2b4f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6565fce0ce26205391184622c264ab18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61e2b743b737b4d8baa6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6387628f6aab287c4810",
+    "url": "/static/js/2.f7dfa004.chunk.js"
+  },
+  {
+    "revision": "207156b889c4d2de9374b944da18f10e",
+    "url": "/static/js/2.f7dfa004.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61e2b743b737b4d8baa6",
+    "url": "/static/js/main.8f79f87d.chunk.js"
+  },
+  {
+    "revision": "ce9f8ec3a9654a7590b1",
+    "url": "/static/js/runtime-main.23ac1c3a.js"
+  }
+]);
